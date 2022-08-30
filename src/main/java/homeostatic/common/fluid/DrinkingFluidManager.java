e4b5530ecb33941a15861e1102b88dd3db64e90e
Wendall Cada <wendallc@83864.com>
2022-08-30 15:05:47 -0700

    Add drinking fluid datapack support.

---
+++ src/main/java/homeostatic/common/fluid/DrinkingFluidManager.java
@@ -0,0 +1,60 @@
+package homeostatic.common.fluid;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import homeostatic.Homeostatic;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class DrinkingFluidManager extends SimpleJsonResourceReloadListener {
+
+    public static final Map<ResourceLocation, DrinkingFluid> FLUIDS = new HashMap<>();
+
+    private static final Gson GSON = new GsonBuilder().registerTypeAdapter(DrinkingFluid.class, new DrinkingFluid.Serializer()).create();
+    private static DrinkingFluidManager INSTANCE;
+
+    public DrinkingFluidManager() {
+        super(GSON, "environment/fluids");
+    }
+
+    public static JsonElement parseWaterItem(DrinkingFluid drinkingFluid) {
+        return GSON.toJsonTree(drinkingFluid);
+    }
+
+    @Override
+    protected void apply(Map<ResourceLocation, JsonElement> pObject, ResourceManager pResourceManager, ProfilerFiller pProfiler) {
+        FLUIDS.clear();
+
+        for (Map.Entry<ResourceLocation, JsonElement> entry : pObject.entrySet()) {
+            try {
+                DrinkingFluid drinkingFluid = GSON.fromJson(entry.getValue(), DrinkingFluid.class);
+
+                FLUIDS.put(drinkingFluid.loc(), drinkingFluid);
+            }
+            catch (Exception e) {
+                Homeostatic.LOGGER.error("Couldn't parse water item %s %s", entry.getKey(), e);
+            }
+        }
+
+        Homeostatic.LOGGER.info("Loaded %d water items", FLUIDS.size());
+    }
+
+    @SubscribeEvent
+    public static void onResourceReload(AddReloadListenerEvent event) {
+        event.addListener(INSTANCE = new DrinkingFluidManager());
+    }
+
+}
--- src/main/java/homeostatic/data/DataGenerators.java
+++ src/main/java/homeostatic/data/DataGenerators.java
@@ -26,6 +26,7 @@ public final class DataGenerators {
         gen.addProvider(true, new ModItemModelProvider(gen, existingFileHelper));
         gen.addProvider(true, new ModBlockStateProvider(gen, existingFileHelper));
         gen.addProvider(true, new RadiationBlocksProvider(gen, Homeostatic.MODID));
+        gen.addProvider(true, new DrinkingFluidsProvider(gen, Homeostatic.MODID));
     }
 
 }
