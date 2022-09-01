64ea394629e1291267c38eb0a14ea665e77091bc
Wendall Cada <wendallc@83864.com>
2022-09-01 12:58:05 -0700

    Add Ecologics hydration items.

--- src/main/java/homeostatic/data/integration/ModIntegration.java
+++ src/main/java/homeostatic/data/integration/ModIntegration.java
@@ -6,6 +6,7 @@ public class ModIntegration {
 
     public static final String BYG_MODID = "byg";
     public static final String CROPTOPIA_MODID = "croptopia";
+    public static final String ECO_MODID = "ecologics";
     public static final String FD_MODID = "farmersdelight";
     public static final String MC_MODID = "minecraft";
     public static final String MORECRAFT_MODID = "morecraft";
@@ -17,6 +18,10 @@ public class ModIntegration {
 
     public static ResourceLocation croptopiaLoc(String path) {
         return new ResourceLocation(CROPTOPIA_MODID, path);
+    }
+
+    public static ResourceLocation ecoLoc(String path) {
+        return new ResourceLocation(ECO_MODID, path);
     }
 
     public static ResourceLocation fdLoc(String path) {
