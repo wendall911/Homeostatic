26d75aa21d5cb4b9a77c83e97f3879027421bb75
Wendall Cada <wendallc@83864.com>
2023-08-02 07:34:15 -0700

    Code cleanup.

--- src/main/java/homeostatic/common/temperature/EnvironmentData.java
+++ src/main/java/homeostatic/common/temperature/EnvironmentData.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 
-import homeostatic.Homeostatic;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
@@ -49,7 +48,6 @@ public class EnvironmentData {
     private double envRadiation;
 
     private static final PerlinSimplexNoise TEMPERATURE_NOISE = new PerlinSimplexNoise(new WorldgenRandom(new LegacyRandomSource(1234L)), ImmutableList.of(0));
-    static final PerlinSimplexNoise FROZEN_TEMPERATURE_NOISE = new PerlinSimplexNoise(new WorldgenRandom(new LegacyRandomSource(3456L)), ImmutableList.of(-2, -1, 0));
 
     /*
      * Returns WBGT
