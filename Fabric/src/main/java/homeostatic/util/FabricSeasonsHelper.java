6b6075819e44efbbf84d8525c141916bb07fbae4
Wendall Cada <wendallc@83864.com>
2024-10-15 10:03:39 -0700

    Add back Fabric Seasons support now that the mod is released!

--- Fabric/src/main/java/homeostatic/util/FabricSeasonsHelper.java
+++ Fabric/src/main/java/homeostatic/util/FabricSeasonsHelper.java
@@ -2,19 +2,16 @@ package homeostatic.util;
 
 import net.minecraft.world.level.Level;
 
-//import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
+import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
 
 public class FabricSeasonsHelper {
 
     public static int getSeasonDuration() {
-        // Hardcoding for now so this  will work if they update
-        //return CONFIG.getSpringLength();
-        return 672000;
+        return CONFIG.getSpringLength();
     }
 
     public static boolean isSeasonDimension(Level level) {
-        return level.dimension() == Level.OVERWORLD;
-        //return CONFIG.isValidInDimension(level.dimension());
+        return CONFIG.isValidInDimension(level.dimension());
     }
 
 }
