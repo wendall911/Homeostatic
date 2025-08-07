01a243502fec4ef63f337a409f097d496da7fcfc
Wendall Cada <wendallc@83864.com>
2025-08-07 11:59:13 -0700

    Change sound source for drinking from flask.

--- Common/src/main/java/homeostatic/event/ClientPlayerEventHandler.java
+++ Common/src/main/java/homeostatic/event/ClientPlayerEventHandler.java
@@ -27,7 +27,7 @@ public class ClientPlayerEventHandler {
         if (hitresult.getType() == HitResult.Type.BLOCK && level.getFluidState(pos).getType() == Fluids.WATER) {
             Services.PLATFORM.getWaterCapabilty(player).ifPresent(data -> {
                 if (data.getWaterLevel() < WaterInfo.MAX_WATER_LEVEL) {
-                    player.level().playSound(player, pos, SoundEvents.GENERIC_DRINK, SoundSource.PLAYERS, 0.4f, 1.0f);
+                    player.level().playSound(player, pos, SoundEvents.GENERIC_DRINK, SoundSource.NEUTRAL, 0.4f, 1.0f);
 
                     Services.CLIENT_PLATFORM.sendDrinkWaterPacket();
                 }
