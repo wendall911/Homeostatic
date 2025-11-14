759d1850bba3218a940ceac361ef8bf0131e3569
Wendall Cada <wendallc@83864.com>
2025-11-14 09:08:38 -0800

    Sync DrinkingFluid data to Fabric client. Fixes #70

--- Fabric/src/main/java/homeostatic/HomeostaticFabric.java
+++ Fabric/src/main/java/homeostatic/HomeostaticFabric.java
@@ -31,6 +31,7 @@ import homeostatic.common.potions.HomeostaticPotions;
 import homeostatic.common.recipe.HomeostaticRecipes;
 import homeostatic.event.ServerEventListener;
 import homeostatic.network.DrinkWater;
+import homeostatic.network.SyncDrinkingFluid;
 import homeostatic.util.WaterHelper;
 
 public class HomeostaticFabric implements ModInitializer {
@@ -51,6 +52,7 @@ public class HomeostaticFabric implements ModInitializer {
         ServerPlayNetworking.registerGlobalReceiver(DrinkWater.TYPE, ((payload, context) -> {
             WaterHelper.drinkWater(context.player());
         }));
+        PayloadTypeRegistry.playS2C().register(SyncDrinkingFluid.TYPE, SyncDrinkingFluid.CODEC);
 
         FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
             builder.registerPotionRecipe(
