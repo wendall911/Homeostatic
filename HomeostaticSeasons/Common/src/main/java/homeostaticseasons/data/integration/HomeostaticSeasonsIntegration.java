2a8343c7879c9c0d143cb6118cdd809be6465929
Wendall Cada <wendallc@83864.com>
2025-12-06 11:13:11 -0800

    Remove Homeostatic Seasons as a module to a separate project.

--- HomeostaticSeasons/Common/src/main/java/homeostaticseasons/data/integration/HomeostaticSeasonsIntegration.java
+++
@@ -1,8 +0,0 @@
-package homeostaticseasons.data.integration;
-
-public class HomeostaticSeasonsIntegration {
-
-    public static final String FS_MODID = "seaons";
-    public static final String SS_MODID = "sereneseasons";
-
-}
